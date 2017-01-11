bb09ed0e80bb7f616ec0acfc1180a919cc1e014e
samuelcorcoran1 <samuel.p.corcoran1@gmail.com>
2017-01-12 10:00:25 +1100

    Testing this comment

--- gen2.py
+++ gen2.py
@@ -30,7 +30,7 @@ class MIDIGenerator(object):
         for noteInfo in notes:
             note = noteInfo[0]
             octave = noteInfo[1]
-            self.MIDIObject.addNote(self.track,self.channel,self.notePitch(note,octave),time,duration,self.volume)
+            self.MIDIObject.addNote(self.track,self.channel,self.notePitch(note,octave),time,duration,30)
 
     def notePitch(self,note,octave):
         return self.notes.index(note) + self.basePitchOfC + (12 * octave)
@@ -330,8 +330,60 @@ spanish = ['C', 'C#',  'E'  ,'F'  ,'G' , 'G#' ,'A#']
 def composeAndWriteToFile(scale,duration,fileName):
     mozart = Composer()
     testMelody = mozart.compose(scale,duration)
+
+    chords = [
+        #Cmaj7
+        [(('C',1),('E',2),('G',1),('B',2)),0,16],
+
+         #Amin7
+        [(('A',1),('C',2),('E',1),('G',1)),16,16], 
+
+        #Emin7
+        [(('E',1),('G',2),('B',1),('D',2)),32,16],
+
+        #Fmaj7
+        [(('F',1),('A',2),('C',1),('E',1)),48,16], 
+
+        #Dmin7
+        [(('D',1),('F',2),('A',1),('C',2)),64,16],
+
+        #GDom7
+        [(('G',1),('B',2),('D',1),('F',1)),80,16],
+
+        #BHalfdim7
+        [(('B',1),('D',2),('F',1),('A',1)),96,16], 
+
+                #Cmaj7
+        [(('C',1),('E',2),('G',1),('B',2)),112,16],
+
+         #Amin7
+        [(('A',1),('C',2),('E',1),('G',1)),128,16], 
+
+        #Emin7
+        [(('E',1),('G',2),('B',1),('D',2)),144,16],
+
+        #Fmaj7
+        [(('F',1),('A',2),('C',1),('E',1)),160,16], 
+
+        #Dmin7
+        [(('D',1),('F',2),('A',1),('C',2)),176,16],
+
+        #GDom7
+        [(('G',1),('B',2),('D',1),('F',1)),192,16],
+
+        #BHalfdim7
+        [(('B',1),('D',2),('F',1),('A',1)),208,16], 
+
+        #Cmaj7
+        [(('C',1),('E',2),('G',1),('B',2)),224,32],
+        ]
+
+    # (note, octave, duration, volume)
+
     MIDIGen = MIDIGenerator(fileName)
     MIDIGen.addMelody(testMelody)
+    for chord in chords:
+        MIDIGen.addChord(chord[0],chord[1],chord[2])
     MIDIGen.writeMidiToFile()
-    
-composeAndWriteToFile(arabScaleNotes,500,"output.mid")
+
+composeAndWriteToFile(majorScaleNotes,500,"output.mid")
